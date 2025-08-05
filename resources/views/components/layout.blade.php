d85817f786f227d06c81a828c0b4405076615f1f
Alex Balak <alex.balak@outlook.com>
2025-08-05 22:20:04 +0200

    added navbar and logos

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -11,16 +11,11 @@
 
 <body>
     <header>
-        <h1>Laravel App</h1>
-        <nav>
-            <a href="/">Home</a>
-            <a href="/register">Register</a>
-        </nav>
+        <x-navbar></x-navbar>
     </header>
     <main class="container">
         {{ $slot }}
     </main>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
-
 </html>
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid justify-content-center">
+    <a class="navbar-brand me-5" href="/">
+      <img src="laravel.svg" alt="Laravel Logo" width="30" height="30" class="d-inline-block align-text-top">
+      Laravel
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/register">Register</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
