af8093cfe113b772aa641209db2caa3b57ff8471
Alex Balak <alex.balak@outlook.com>
2025-08-05 21:38:25 +0200

    added validation to UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,8 +6,13 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function register()
+    public function register(Request $request)
     {
-        return "Hello from UserController's register method!";
+        $incomingFields = $request->validate([
+            'name' => 'required|max:50',
+            'email' => 'required|email',
+            'password' => 'required|min:6|max:50',
+        ]);
+        return "Hello from UserController";
     }
 }
