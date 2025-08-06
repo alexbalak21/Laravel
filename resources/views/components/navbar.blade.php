f6747389643c728fb1e5adde30827724e4238526
Alex Balak <alex.balak@outlook.com>
2025-08-06 23:57:51 +0200

    navbar update

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid justify-content-center">
+  <div class="container-fluid">
     <a class="navbar-brand me-5" href="/">
       <img src="laravel.svg" alt="Laravel Logo" width="30" height="30" class="d-inline-block align-text-top">
       {{ $title ?? 'Laravel' }}
@@ -8,7 +8,7 @@
       aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+    <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="/">Home</a>
