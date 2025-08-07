914c0b60011529b31f77bd0ffa40c6a38fed509d
Alex Balak <alex.balak@outlook.com>
2025-08-07 16:04:28 +0200

    created dropdown user menu

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,22 +1,34 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
+<nav class="navbar navbar-expand-lg navbar-light bg-light mx-2">
     <a class="navbar-brand me-5" href="/">
-      <img src="laravel.svg" alt="Laravel Logo" width="30" height="30" class="d-inline-block align-text-top">
-      {{ $title ?? 'Laravel' }}
+        <img src="laravel.svg"
+            alt="Laravel Logo"
+            width="30"
+            height="30"
+            class="d-inline-block align-text-top">
+        {{ $title ?? 'Laravel' }}
     </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+
+    <x-userMenu />
+
+    <button class="navbar-toggler ms-1"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
     </button>
+
     <div class="collapse navbar-collapse text-center" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/register">Register</a>
-        </li>
-      </ul>
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link active" href="/">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/register">Register</a>
+            </li>
+        </ul>
     </div>
-  </div>
+
 </nav>
